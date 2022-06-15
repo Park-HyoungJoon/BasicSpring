b94d89456dc5954c1e0f16606c281ba55fa3414d
MinJu-A <pinokoiandmj@naver.com>
2022-06-15 13:07:47 +0900

    메타버스로 바로 가는 주소 추가 + 프론트 캐러셀 앞면 수정 + footer 수정 + 구글 폼 입력 완료 + 서버 열어서 값 보낸 후 읽어 올 예정

--- src/main/java/kr/inhatc/spring/mataverse/controller/MetaverseController.java
+++ src/main/java/kr/inhatc/spring/mataverse/controller/MetaverseController.java
@@ -1,19 +1,11 @@
 package kr.inhatc.spring.mataverse.controller;
 
-import java.security.Principal;
+import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import kr.inhatc.spring.login.entity.Member;
-import kr.inhatc.spring.login.repository.UserRepositoy;
 
 @Controller
 public class MetaverseController{
@@ -23,6 +15,12 @@ public class MetaverseController{
 	@RequestMapping("/metaverse")
 	public String metabusMain() {
 		return "metaverse/metaverseMain";
+	}
+	
+//	메타버스 수업
+	@RequestMapping("/metaverseStudy")
+	public void metarverseStudy(HttpServletResponse httpServletResponse) throws IOException {
+		httpServletResponse.sendRedirect("https://app.gather.town/app/Jbil2l6iorarf1T4/My%20E-do%20Class");
 	}
 	
 
